bbd0269178b0f2536d7c103762845424f452eae4
Francesco Sica <fsica171@gmail.com>
2021-03-04 00:50:52 +0100

    Terminato parte codice.

--- assets/main.js
+++ assets/main.js
@@ -4,19 +4,33 @@ var mailAutorizzate = ["fsica171@gmail.com", "tizio@gmail.com", "boolean@gmail.c
 var mailInput = prompt("Inserisci la mail");
 
 for (var i = 0; i < mailAutorizzate.length; i++) {
-    var checkMail = 0;
-    var pointBreak = 0;
+    var find_Mail = false;
+    var pointBreak = false;
 
     if (mailInput == mailAutorizzate[i]) {
-        checkMail = 1;
-        pointBreak = 1;
+        find_Mail = true;
+        pointBreak = true;
         console.log("Mail trovata");
 
-        if (pointBreak == 1) {
-            break;
+        if (pointBreak == true) {
+            i = --mailAutorizzate.length;
         }
 
-    }else if(checkMail == 0 && i == mailAutorizzate.length -1){
+    }else if(find_Mail == false && i == mailAutorizzate.length -1){
         console.log("Mail non trovata");
     }
+}
+
+
+// Dadi
+var giocatore = Math.floor(Math.random() * 6 + 1);
+
+var banco = Math.floor(Math.random() * 6 + 1);
+
+if(giocatore > banco){
+    console.log("Giocatore vince con " + giocatore);
+}else if (banco > giocatore) {
+    console.log("Banco vince con " + banco);
+}else {
+    console.log("Pareggio \nbanco : "  + banco + "\nGiocatore : " + giocatore);
 }
